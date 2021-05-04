052eb0d643c2fe64d048dfad4a3883160dc0b4a5
Xueqi Guan <xueqig@student.unimelb.edu.au>
2021-05-04 10:51:55 +1000

    read file

--- phase1.c
+++ phase1.c
@@ -1,4 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
 int main(int argc, char *argv[])
 {
-    return 0;
+    // printf("%s\n", argv[1]);
+
+    char buf[100];
+    size_t nbytes;
+    ssize_t bytes_read;
+    nbytes = sizeof(buf);
+    bytes_read = read(STDIN_FILENO, buf, nbytes);
+
+    printf("buf[0]: %c\n", buf[0]);
+    printf("buf: %s\n", buf);
+    printf("nbytes: %zu\n", nbytes);
 }
