cf56877d908f2116b8a4ba90a584a62562353ace
Xueqi Guan <xueqig@student.unimelb.edu.au>
2021-05-17 22:05:02 +1000

    continue reading

--- main.c
+++ main.c
@@ -88,39 +88,46 @@ int main(int argc, char *argv[])
         }
 
         // Read characters from the connection, then process
-        n = read(newsockfd, buf, 255); // n is number of characters read
+        n = read(newsockfd, req_buf, 255); // n is number of characters read
         if (n < 0)
         {
             perror("read");
             exit(EXIT_FAILURE);
         }
 
-        // Continue reading until receive whole request
-        int req_buf_idx = 0;
-        while (1)
+        int req_buf_len = (int)req_buf[1];
+
+        while (n != req_buf_len)
         {
-            for (i = 0; i < n; i++)
-            {
-                req_buf[req_buf_idx++] = buf[i];
-            }
-            if (req_buf_idx < (int)req_buf[1] + 2)
-            {
-                printf("total length: %d\n", (int)req_buf[1]);
-                n = read(newsockfd, buf, 255); // n is number of characters read
-                if (n < 0)
-                {
-                    perror("read");
-                    exit(EXIT_FAILURE);
-                }
-            }
-            else
-            {
-                break;
-            }
+            n += read(newsockfd, buf + n, 255);
         }
 
+        // // Continue reading until receive whole request
+        // int req_buf_idx = 0;
+        // while (1)
+        // {
+        //     for (i = 0; i < n; i++)
+        //     {
+        //         req_buf[req_buf_idx++] = buf[i];
+        //     }
+        //     if (req_buf_idx < (int)req_buf[1] + 2)
+        //     {
+        //         printf("total length: %d\n", (int)req_buf[1]);
+        //         n = read(newsockfd, buf, 255); // n is number of characters read
+        //         if (n < 0)
+        //         {
+        //             perror("read");
+        //             exit(EXIT_FAILURE);
+        //         }
+        //     }
+        //     else
+        //     {
+        //         break;
+        //     }
+        // }
+
         // Null-terminate string
-        req_buf[req_buf_idx] = '\0';
+        req_buf[n] = '\0';
 
         printf("req buf: \n");
         for (i = 0; i < n; i++)
